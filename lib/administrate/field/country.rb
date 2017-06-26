cee4706940a2f0be77dbd7547d0055254248ae60
Martin Ulleberg <martin.ulleberg@gmail.com>
2017-06-26 16:46:59 +0200

    Adds priority_countries and include_blank as options

--- lib/administrate/field/country.rb
+++ lib/administrate/field/country.rb
@@ -9,6 +9,14 @@ module Administrate
       def to_s
         data
       end
+
+      def priority_countries
+        options.fetch(:priority_countries, %w(US IT GB)
+      end
+
+      def include_blank?
+        options.fetch(:include_blank, true)
+      end
     end
   end
 end
